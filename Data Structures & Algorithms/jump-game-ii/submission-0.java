946834e2994a21e27d6dfe3044270e7cbcfa6b69
varsh-97 <vramesh1@umbc.edu>
2026-06-13 20:01:26 -0500

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int jump(int[] nums) {
+        // jumping from j->1: dp[i] = min jumps required to get to j + 1, so, dp[j]+1
+        // at every index, calculate the number of jumps we can do from that index and if the res index is n-1
+        // fill the dp[] with max int and return the dp[n-1]
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            int val = nums[i]; // total jumps from that index
+            for(int j=1; j<=val; j++)
+            {
+                if(i+j < nums.length)
+                {
+                    dp[i+j] = Math.min(dp[i+j], dp[i]+1);
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
